bd1d1d30645b5b31004b1bf504cd8ae5fe585a24
Kumail786 <kumailrawjani900@gmail.com>
2019-12-21 00:35:15 +0500

    generating pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,9 +6,36 @@ module.exports.onCreateNode = ({ node, actions }) => {
     const slug = path.basename(node.fileAbsolutePath, ".md")
     console.log("@@@@@@@@@@", slug)
     createNodeField({
-        node,
-        name : "slug",
-        value : slug
+      node,
+      name: "slug",
+      value: slug,
     })
   }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogTemplate = path.resolve("./src/templates/blog.js")
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  res.data.allMarkdownRemark.edges.map(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
