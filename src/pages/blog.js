81948a6f9842e77cfb1107a880725f887c763e93
Kumail786 <kumailrawjani900@gmail.com>
2019-12-21 00:57:17 +0500

    displaying markdown content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,46 +1,42 @@
 import React from "react"
 import Layout from "../components/layout"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
-  query{
-    allMarkdownRemark{
-      edges{
-        node{
-          frontmatter{
-            title,
-            date
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields{
+              slug
+            }
           }
-         
-          
-         
         }
       }
-    
     }
-  }
   `)
-  
+
   return (
     <Layout>
       <ol>
-    {data && data.allMarkdownRemark.edges.map(edge=>{
-return(
-  <li>
-    <h2>{edge.node.frontmatter.title}</h2>
-<p>{edge.node.frontmatter.date}</p>
-  </li>
-)
-    })
-
-
-    }
-    </ol>
-      <h1>Blog Page</h1>
-      <p>Post will show here</p>
+        {data &&
+          data.allMarkdownRemark.edges.map(edge => {
+            return (
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+              <li>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
+              </li>
+              </Link>
+            )
+          })}
+      </ol>
     </Layout>
   )
- 
 }
 
 export default BlogPage
